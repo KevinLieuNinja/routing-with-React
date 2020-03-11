1909ac55f88b4f7921e0f0d1dd727c582254bbc0
KevinLieuNinja <klieu925@gmail.com>
2020-03-11 15:48:19 -0700

    fixed

---
+++ src/components/num.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Number =( {str,color,colorText} ) =>{
+    const style ={
+        backgroundColor: color,
+        color: colorText
+    }
+    return(
+        <div style = {style}>
+            <h1> word is {str}</h1>
+            
+        </div>
+    )
+}
+export default Number;
